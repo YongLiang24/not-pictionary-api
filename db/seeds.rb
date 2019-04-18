3ee7925e880e36b2ff72a5b8575e37df7a1ad3c8
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-04-18 13:53:30 -0700

    Add new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,9 @@ game_one = Game.find_or_create_by(drawer: pizza, guesser: roni, answer: 'Pie', g
 game_two = Game.find_or_create_by(drawer: nneka, guesser: king, answer: 'Slice of Cake', guesses: [], name: 'Family Dinner')
 game_three = Game.find_or_create_by(drawer: pizza, guesser: obinna, answer: 'Love', guesses: [], name: 'Jazzagals')
 game_four = Game.find_or_create_by(drawer: roni, guesser: carrie, answer: 'Ace of Spades', guesses: [], name: 'Moira vs Town Council')
+game_four = Game.find_or_create_by(drawer: roni, guesser: carrie, answer: 'Ace of Spades', guesses: [], name: 'Moira vs Town Council')
+
+
 
 game_one.is_active = true
 game_one.save
